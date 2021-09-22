ce9379553b5a4365e6509af5f5a0c3368b0c3dbf
cauamenezes <cauamenezes.07@gmail.com>
2021-09-22 10:31:16 -0300

    imagens inseridas automaticamente através de js

---
+++ galeria.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const imagens = [
+  "./img/mario.png",
+  "./img/luigi.png",
+  "./img/peach.png",
+  "./img/browser.png",
+  "./img/yoshi.png",
+  "./img/koopa-troopa.png",
+  "./img/wario.png",
+  "./img/waluigi.png",
+];
+
+// MAP - FILTER - REDUCE -> Declarativa
+
+const criarItem = (urlImagem) => {
+  const container = document.querySelector(".galeria-container");
+
+  const novoLink = document.createElement("a");
+  novoLink.href = urlImagem;
+  novoLink.classList.add("galeria-items")
+  novoLink.innerHTML = `<img src="${urlImagem}" alt="mario"></img>`
+
+  container.appendChild(novoLink)
+
+  //   container.innerHTML += `
+  //     <a href="${urlImagem}" class="galeria-items">
+  //         <img src="${urlImagem}" alt="mario">
+  //     </a>
+  //     `;
+};
+
+const carregarImagens = () => imagens.forEach(criarItem);
+
+carregarImagens();
